46a869383e997825af28a5f412a2d8957caa7f8a
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2025-09-19 22:44:27 +0600

    feat refactor code. basic structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
-import 'package:pascaline_scientific_calculator/data/notifier.dart';
 
+import 'data/notifier.dart';
 import 'views/widget_tree.dart';
 
-Future main() async {
-  await dotenv.load(fileName: ".env");
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MainApp());
-}
+void main() => runApp(const MainApp());
 
 class MainApp extends StatefulWidget {
   const MainApp({super.key});
@@ -19,21 +14,17 @@ class MainApp extends StatefulWidget {
 
 class _MainAppState extends State<MainApp> {
   @override
-  Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      valueListenable: themeMode,
-      builder: (context, themeMode, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
-            brightness: themeMode == ThemeMode.dark
-                ? Brightness.dark
-                : Brightness.light,
-          ),
-          home: WidgetTree(),
-        );
-      },
-    );
-  }
+  Widget build(BuildContext context) => ValueListenableBuilder(
+    valueListenable: themeMode,
+    builder: (context, themeMode, child) => MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        brightness: themeMode == ThemeMode.dark
+            ? Brightness.dark
+            : Brightness.light,
+      ),
+      home: WidgetTree(),
+    ),
+  );
 }
--- lib/views/widget_tree.dart
+++ lib/views/widget_tree.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:pascaline_scientific_calculator/views/pages/calculator.dart';
 
 class WidgetTree extends StatelessWidget {
   const WidgetTree({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    throw UnimplementedError();
-  }
+  Widget build(BuildContext context) => Scaffold(body: Calculator());
 }
