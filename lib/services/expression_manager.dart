d5c1fa5bbf119653ba022db7eb53ddabf0b0e519
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2025-09-20 23:24:58 +0600

    expression_manager class

--- lib/services/expression_manager.dart
+++ lib/services/expression_manager.dart
@@ -1,1 +1,35 @@
+import 'operand.dart';
+import 'expression.dart';
+import 'operator.dart';
 
+class ExpressionManager {
+  InfixExpression _infixExpression;
+  StringBuffer _numberBuffer;
+
+  ExpressionManager._(this._infixExpression, this._numberBuffer);
+
+  factory ExpressionManager() {
+    return ExpressionManager._(InfixExpression(), StringBuffer());
+  }
+
+  bool _registerNumbers() {
+    if (_numberBuffer.isEmpty) {
+      return false;
+    }
+    _infixExpression.addToken(Operand(_numberBuffer.toString()));
+    _numberBuffer.clear();
+    return true;
+  }
+
+  void allClear() {
+    _infixExpression.clear();
+    _numberBuffer.clear();
+  }
+
+  void addOperator(IOperator op) {
+    _infixExpression.addToken(op);
+    _registerNumbers();
+  }
+
+  num calculate() => PostfixExpression(_infixExpression).evaluate();
+}
